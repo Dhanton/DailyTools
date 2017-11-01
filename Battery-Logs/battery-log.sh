a8fafdbc4aaf8c06de386ff3182581636787af22
dhanton <dhantonrevolution@gmail.com>
2017-11-01 12:14:30 +0100

    Add charging/discharging info to log file

--- Battery-Logs/battery-log.sh
+++ Battery-Logs/battery-log.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+is_charging=$(acpi | grep Charging)
+
+# Add differente prefix to log file depending if charging or not
+if [[ $is_charging = "" ]]; then
+	prefix=$'dis'
+else
+	prefix=$'char'
+fi
+
 log_date=$(date '+%d_%m_%Y-%H_%M')
 
-upower -i /org/freedesktop/UPower/devices/battery_BAT1 > ~/.battery-logs/$log_date
+# Write log file
+upower -i /org/freedesktop/UPower/devices/battery_BAT1 > ~/.battery-logs/$prefix-$log_date
